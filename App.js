0c9c09b246c4872d31ce9a0f5686dbb30668bf81
Abhishek Patel <abhipatelpatel1081@gmail.com>
2026-01-20 18:39:04 +0530

    Add files via upload
    Complete Task

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PostList from "./pages/PostList.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <PostList/>
+    </div>
+  );
+}
+export default App;
